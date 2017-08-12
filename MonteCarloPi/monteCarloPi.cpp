45b8e32d309a5cbf58f2e1e70f3ac4d5302c9a23
G-nther <guenther.hackl@gmx.at>
2017-08-12 10:58:37 +0200

    implement writing to file

--- MonteCarloPi/monteCarloPi.cpp
+++ MonteCarloPi/monteCarloPi.cpp
@@ -2,13 +2,15 @@
 #include <ctime>
 #include <cstdlib>
 #include <string>
+#include <fstream>
 
-double montePi(int pointNumber);
+double montePi(int pointNumber, bool writePoints);
 
 int main(int argc, char* argv[])
 {
 	time_t start = time(NULL);
-	int points = 1000000;
+	int points = 10000000;
+	bool writePoints = false;
 
 	for (int i = 1; i < argc; i++)
 	{
@@ -33,22 +35,32 @@ int main(int argc, char* argv[])
 				return 1;
 			}
 		}
+		if (strcmp(argv[i], "-f") || strcmp(argv[i], "--file"))
+		{
+			writePoints = true;
+		}
 	}
 
-	double pi = montePi(points);
+	double pi = montePi(points, writePoints);
 
 	std::cout << "Estimation for Pi:\t" << pi << "\nElapsed time:\t\t" << (long)(time(NULL) - start) << " second(s)";
-
 	return 0;
 }
 
-double montePi(int pointNumber)
+double montePi(int pointNumber, bool writePoints)		//returns estimation of Pi
 {
 	double x = 0.0, y = 0.0;
 	double r = 0.0, ratio = 0.0;
 	int pointsInside = 0;
+	std::ofstream pointLog;
 
 	srand(time(NULL));
+
+	if (writePoints)
+	{
+		pointLog.open("points.dat");
+		pointLog << "x;y;inCircle\n";
+	}
 
 	for (int i = 0; i < pointNumber; i++)
 	{
@@ -60,7 +72,14 @@ double montePi(int pointNumber)
 		if (r <= 1) {							//if the distance is <= 1 the point is inside the quater-circle
 			pointsInside++;
 		}
+
+		if (writePoints)
+			pointLog << x << ";" << y << ";" << (r <= 1) << "\n";
+
 	}
+
+	if (writePoints)
+		pointLog.close();
 
 	ratio = (double)pointsInside / pointNumber;		//the ration between the quater circle with radius = 1 and the square with sidelength = 1 is pi/4 and 
 													//should be approached by the ratio of pointsInside and total points
